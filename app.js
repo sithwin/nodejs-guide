341aa36ae6b86af64b063086236c4def34211f42
Si Thu Win <sthuwin@gmail.com>
2021-01-21 22:18:04 +0800

    write to file sync

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
     return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message, (err) => {
+      fs.writeFile("message.txt", message, (err) => {
         res.statusCode = 302;
         res.setHeader("Location", "/");
         return res.end();
