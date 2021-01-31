22eabc71dd0612d9190f564efd8a07fa7ab6b73b
Si Thu Win <sthuwin@gmail.com>
2021-01-31 12:47:12 +0800

    replace with express

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
 
-const server = http.createServer(routes.handler);
+const app = express();
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  next();
+});
 
-server.listen(3002);
+app.use((req, res, next) => {
+  console.log("in another middleware");
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3002);
--- readme.md
+++ readme.md
@@ -12,3 +12,9 @@ npm install nodemon -g
 More on debugging:
 [nodeJs](https://nodejs.org/en/docs/guides/debugging-getting-started/)
 [vsCode](https://code.visualstudio.com/docs/nodejs/nodejs-debugging)
+
+# Extension
+
+- Material Icon Theme
+- Pair Colorize
+- Spell checker
